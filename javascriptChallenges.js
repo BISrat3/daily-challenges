0d4b3e3775fb1e937a22a97a6c8390b34d56f917
bisrat <bisrat.amtataw@gmail.com>
2022-07-23 00:13:07 -0400

    jul22

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -450,8 +450,28 @@ const { Query } = require("mongoose")
 
 // factorial(6)
 
-function factorial(num){
-    if (num === 1) return 1;
-    return num * factorial(num-1)
+// function factorial(num){
+//     if (num === 1) return 1;
+//     return num * factorial(num-1)
+// }
+// factorial(3)
+
+
+// Helper method recursion 
+function collectOddValues(arr){
+    let result = [];
+    function helper(helperInput){
+        if (helperInput.length===0){
+            return;
+        }
+        if (helperInput[0]% 2 !==0){
+            result.push(helperInput[0])
+        }
+        helper(helperInput.slice(1))
+    }
+    helper(arr)
+    console.log(result)
+    return result;
 }
-factorial(3)
+
+collectOddValues([1,2,3,4,5,6,7,8,9])
