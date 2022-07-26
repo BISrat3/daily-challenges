6b83b76d107595850208c74d399171940a9fcd9b
bisrat <bisrat.amtataw@gmail.com>
2022-07-25 20:51:58 -0400

    jul25

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -717,8 +717,39 @@ const { Query } = require("mongoose")
 
 // console.log(mergeTwoLists([], [0]))
 
+// Merge sort
+// function mergeSolution (arr1, arr2){
+//     let results =[];
+//     let i=0;
+//     let j=0;
+//     while(i<arr1.length && j< arr2.length){
+//         if (arr2[j]> arr1[i]){
+//             results.push(arr1[i])
+//             i++
+//         }
+//         else{
+//             results.push(arr2[j])
+//             j++
+//         }
+//     }
+//     while(i< arr1.length){
+//         results.push(arr1[i])
+//         i++
+//     }
+//     while(j< arr2.length){
+//         results.push(arr2[j])
+//         j++
+//     }
+//     console.log(results)
+//     return results;
 
-function mergeSolution (arr1, arr2){
+// }
+
+// mergeSolution([1,10,50], [2,14,99,100])
+
+
+// 
+function merge(arr1, arr2){
     let results =[];
     let i=0;
     let j=0;
@@ -745,4 +776,15 @@ function mergeSolution (arr1, arr2){
 
 }
 
-mergeSolution([1,10,50], [2,14,99,100])
+function mergeSort(arr){
+    if (arr.length <=1 )
+    return arr;
+    // mergeSort()
+    let mid = Math.floor (arr.length/2)
+    let left = mergeSort(arr.slice(0,mid))
+    let right= mergeSort(arr.slice(mid))
+    return merge(left, right)
+
+}
+
+mergeSort([10,24,76,73])
