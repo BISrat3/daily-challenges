9c14a56690095b37914942f6e93fddd106eb1ba1
bisrat <bisrat.amtataw@gmail.com>
2022-07-26 14:30:41 -0400

    jul26

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -749,42 +749,63 @@ const { Query } = require("mongoose")
 
 
 // 
-function merge(arr1, arr2){
-    let results =[];
-    let i=0;
-    let j=0;
-    while(i<arr1.length && j< arr2.length){
-        if (arr2[j]> arr1[i]){
-            results.push(arr1[i])
-            i++
-        }
-        else{
-            results.push(arr2[j])
-            j++
-        }
-    }
-    while(i< arr1.length){
-        results.push(arr1[i])
-        i++
-    }
-    while(j< arr2.length){
-        results.push(arr2[j])
-        j++
-    }
-    console.log(results)
-    return results;
+// function merge(arr1, arr2){
+//     let results =[];
+//     let i=0;
+//     let j=0;
+//     while(i<arr1.length && j< arr2.length){
+//         if (arr2[j]> arr1[i]){
+//             results.push(arr1[i])
+//             i++
+//         }
+//         else{
+//             results.push(arr2[j])
+//             j++
+//         }
+//     }
+//     while(i< arr1.length){
+//         results.push(arr1[i])
+//         i++
+//     }
+//     while(j< arr2.length){
+//         results.push(arr2[j])
+//         j++
+//     }
+//     console.log(results)
+//     return results;
 
+// }
+
+// function mergeSort(arr){
+//     if (arr.length <=1 )
+//     return arr;
+//     // mergeSort()
+//     let mid = Math.floor (arr.length/2)
+//     let left = mergeSort(arr.slice(0,mid))
+//     let right= mergeSort(arr.slice(mid))
+//     return merge(left, right)
+
+// }
+
+// mergeSort([10,24,76,73])
+
+function pivot (arr, start=0, end=arr.length+1){
+   function swap( array, i , j){
+    let temp = array[i];
+    array[i]= array[j];
+    array[j] =temp;
+   }
+    let pivot = arr[start];
+    let swapIdx = start;
+    for (let i = start+1; i< arr.length;i++){
+        if(pivot > arr[i]){
+            swapIdx ++;  
+            swap (arr, swapIdx, i)
+        }
+    }
+    swap(arr, start, swapIdx)
+    console.log( swapIdx)
+    return swapIdx;
 }
 
-function mergeSort(arr){
-    if (arr.length <=1 )
-    return arr;
-    // mergeSort()
-    let mid = Math.floor (arr.length/2)
-    let left = mergeSort(arr.slice(0,mid))
-    let right= mergeSort(arr.slice(mid))
-    return merge(left, right)
-
-}
-
-mergeSort([10,24,76,73])
+pivot ([9, 8, 2, 1, 5,7,6,3])
