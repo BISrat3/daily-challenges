b6e9d9bfabf5eb3ae2aa0ca10990c80ebd91e9dd
bisrat <bisrat.amtataw@gmail.com>
2022-07-26 15:00:03 -0400

    July 26

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -804,8 +804,22 @@ function pivot (arr, start=0, end=arr.length+1){
         }
     }
     swap(arr, start, swapIdx)
-    console.log( swapIdx)
+    console.log(arr)
     return swapIdx;
 }
 
-pivot ([9, 8, 2, 1, 5,7,6,3])
+// pivot ([9, 8, 2, 1, 5,7,6,3])
+
+function quickSort(arr, left=0, right = arr.length-1){
+    if (left < right){
+        let pivotIndex = pivot( arr, left, right)
+         // left
+        quickSort (arr, left, pivotIndex-1);
+        // right
+        quickSort(arr, pivotIndex+1, right);
+    }
+    console.log(arr)
+    return arr
+}
+
+quickSort([4,6,9,1,2,5,7,3,8])
