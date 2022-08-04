792721122bef6ab65a306955cbe47d7b1f77fb7b
bisrat <bisrat.amtataw@gmail.com>
2022-08-04 15:58:22 -0400

    Aug 4

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -1776,7 +1776,29 @@ class BreadthFirstSearch{
         traverse(current)
         return data;
     }
-  
+    // DFS PostOrder
+    // create a varaible to store the values of nodes visited 
+    // Store the root of the BST in a variable called current
+    // Write a helper function which accepts a node
+    
+    // - IF the node has a left property,call the helper function with the left property on the node
+    // -If the node has a right property, call the helper function with the right property on the node
+    // - Push the value of the node to the variable that stores the values
+    // invoke the helper function with the current variable
+    // Return the array of values
+    DFSPostOrder(){
+        let data =[];
+        let current = this.root;
+        function traverse(node){
+            if(node.left)
+            traverse(node.left)
+            if(node.right)
+            traverse(node.right)
+            data.push(node)
+        }
+        traverse(current)
+        return data;
+    }
 }
 
 let tree = new BreadthFirstSearch();
@@ -1788,3 +1810,4 @@ tree.insert(8);
 tree.insert(20);
 console.log(tree.DFSPreOrder())
 // console.log(tree.BFS())
+console.log(tree.DFSPostOrder())
