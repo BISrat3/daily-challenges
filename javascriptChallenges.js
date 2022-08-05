03b61a96cc40d27359845437625928364fd5bbae
bisrat <bisrat.amtataw@gmail.com>
2022-08-05 13:38:17 -0400

    Aug 5

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -1696,142 +1696,182 @@ const { Query } = require("mongoose")
 // If there is a left property on the node dequeued - add it to the queue
 // If there is a right property on the node dequeued -add it to the queue
 // Return the varuable that stores the values
-class Node{
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-class BreadthFirstSearch{
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+// class BreadthFirstSearch{
+//     constructor(){
+//         this.root = null;
+//     }
+//     insert(value){
+//                 let newNode = new Node(value);
+//                 if(this.root === null){
+//                     this.root = newNode;
+//                     return this;
+//                 }
+//                 else {
+//                     let current = this.root;
+//                     while(true){
+//                         if (value === current.value) return undefined;
+//                         if (value < current.value){
+//                             if (current.left === null){
+//                                 current.left = newNode;
+//                                 return this;
+//                             }
+//                             else{
+//                                 current = current.left;
+//                             }
+//                         }
+//                         else if(value > current.value){
+//                             if (current.right === null){
+//                                 current.right = newNode;
+//                                 return this;
+//                             }
+//                             else{
+//                                 current = current.right
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//     BFS(){
+//         let node = this.root, 
+//             data = [],
+//             queue = [];
+//         queue.push(node);
+//         while (queue.length){
+//             node = queue.shift();
+//             data.push(node.value);
+//             if (node.left) queue.push(node.left);
+//             if (node.right) queue.push(node.right);
+//         }
+//         return data;
+//     }
+//     // DFS preorder
+//     // create a varaible to store the values of nodes visited 
+//     // Store the root of the BEST in a variable called current
+//     // Write a helper function which accepts a node
+//     // - Push the value of the node to the variable that stores the values
+//     // - IF the node has a left property,call the helper function with the left property on the node
+//     // -If the node has a right property, call the helper function with the right property on the node
+//     // invoke the helper function with the current variable
+//     // Return the array of values
+
+//     DFSPreOrder(){
+//         let data = []
+//         let current = this.root;
+//         function traverse(node){
+//             data.push(node.value)
+//             if(node.left){
+//                 traverse(node.left)
+//             }
+//             if(node.right){
+//                 traverse(node.right)
+//             }
+//         }
+//         traverse(current)
+//         return data;
+//     }
+//     // DFS PostOrder
+//     // create a varaible to store the values of nodes visited 
+//     // Store the root of the BST in a variable called current
+//     // Write a helper function which accepts a node
+    
+//     // - IF the node has a left property,call the helper function with the left property on the node
+//     // -If the node has a right property, call the helper function with the right property on the node
+//     // - Push the value of the node to the variable that stores the values
+//     // invoke the helper function with the current variable
+//     // Return the array of values
+//     DFSPostOrder(){
+//         let data =[];
+//         let current = this.root;
+//         function traverse(node){
+//             if(node.left)
+//             traverse(node.left)
+//             if(node.right)
+//             traverse(node.right)
+//             data.push(node.value)
+//         }
+//         traverse(current)
+//         return data;
+//     }
+//     // DFS InOrder
+//     // create a varaible to store the values of nodes visited 
+//     // Store the root of the BST in a variable called current
+//     // Write a helper function which accepts a node
+    
+//     // - IF the node has a left property,call the helper function with the left property on the node
+//     // - Push the value of the node to the variable that stores the values
+//     // -If the node has a right property, call the helper function with the right property on the node
+//     // invoke the helper function with the current variable
+//     // Return the array of values
+//     DFSInOrder(){
+//         let data =[];
+//         let current = this.root;
+//         function traverse(node){
+//             if(node.left)
+//             traverse(node.left)
+//             data.push(node.value)
+//             if(node.right)
+//             traverse(node.right)
+//         }
+//         traverse(current)
+//         return data;
+//     }
+// }
+
+// let tree = new BreadthFirstSearch();
+// tree.insert(10);
+// tree.insert(6);
+// tree.insert(15);
+// tree.insert(3);
+// tree.insert(8);
+// tree.insert(20);
+// console.log(tree.DFSPreOrder())
+// // console.log(tree.BFS())
+// console.log(tree.DFSPostOrder())
+// console.log(tree.DFSInOrder())
+
+// Insert Pseudocode
+// Push the value into the values property on the heap
+//  Bubble up: - Create a variable called index which is the length of the values property-1
+// - Create a variable called parentIndex which is the floor of(index-1)/2
+// - Keep looping as long as the values element at the parentIndex is less than the values element at the child index
+// - Swap the vlaue of the values element at the parentIndex with the value of the element proprty at the child index
+// - Set the index to be the parentIndex, and start over!
+
+class MaxBinaryHeap{
     constructor(){
-        this.root = null;
+        this.values= [41,39,33,18,27,12];
     }
-    insert(value){
-                let newNode = new Node(value);
-                if(this.root === null){
-                    this.root = newNode;
-                    return this;
-                }
-                else {
-                    let current = this.root;
-                    while(true){
-                        if (value === current.value) return undefined;
-                        if (value < current.value){
-                            if (current.left === null){
-                                current.left = newNode;
-                                return this;
-                            }
-                            else{
-                                current = current.left;
-                            }
-                        }
-                        else if(value > current.value){
-                            if (current.right === null){
-                                current.right = newNode;
-                                return this;
-                            }
-                            else{
-                                current = current.right
-                            }
-                        }
-                    }
-                }
-            }
-    BFS(){
-        let node = this.root, 
-            data = [],
-            queue = [];
-        queue.push(node);
-        while (queue.length){
-            node = queue.shift();
-            data.push(node.value);
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }  
+    bubbleUp(){
+        let index = this.values.length-1;
+        const element = this.values[index]
+        while(index>0){
+            let parentIndex = Math.floor((index-1)/2)
+            console.log(parentIndex)
+            let parent = this.values[parentIndex];
+            // if(element > parent){
+            //     this.values[parentIndex]= element;
+            //     this.values[index] = parent;
+            //     index = parentIndex;}
+            if(element <= parent) break
+                this.values[parentIndex]= element;
+                this.values[index] = parent;
+                index = parentIndex;
         }
-        return data;
-    }
-    // DFS preorder
-    // create a varaible to store the values of nodes visited 
-    // Store the root of the BEST in a variable called current
-    // Write a helper function which accepts a node
-    // - Push the value of the node to the variable that stores the values
-    // - IF the node has a left property,call the helper function with the left property on the node
-    // -If the node has a right property, call the helper function with the right property on the node
-    // invoke the helper function with the current variable
-    // Return the array of values
-
-    DFSPreOrder(){
-        let data = []
-        let current = this.root;
-        function traverse(node){
-            data.push(node.value)
-            if(node.left){
-                traverse(node.left)
-            }
-            if(node.right){
-                traverse(node.right)
-            }
-        }
-        traverse(current)
-        return data;
-    }
-    // DFS PostOrder
-    // create a varaible to store the values of nodes visited 
-    // Store the root of the BST in a variable called current
-    // Write a helper function which accepts a node
-    
-    // - IF the node has a left property,call the helper function with the left property on the node
-    // -If the node has a right property, call the helper function with the right property on the node
-    // - Push the value of the node to the variable that stores the values
-    // invoke the helper function with the current variable
-    // Return the array of values
-    DFSPostOrder(){
-        let data =[];
-        let current = this.root;
-        function traverse(node){
-            if(node.left)
-            traverse(node.left)
-            if(node.right)
-            traverse(node.right)
-            data.push(node.value)
-        }
-        traverse(current)
-        return data;
-    }
-    // DFS InOrder
-    // create a varaible to store the values of nodes visited 
-    // Store the root of the BST in a variable called current
-    // Write a helper function which accepts a node
-    
-    // - IF the node has a left property,call the helper function with the left property on the node
-    // - Push the value of the node to the variable that stores the values
-    // -If the node has a right property, call the helper function with the right property on the node
-    // invoke the helper function with the current variable
-    // Return the array of values
-    DFSInOrder(){
-        let data =[];
-        let current = this.root;
-        function traverse(node){
-            if(node.left)
-            traverse(node.left)
-            data.push(node.value)
-            if(node.right)
-            traverse(node.right)
-        }
-        traverse(current)
-        return data;
-    }
+    } 
 }
-
-let tree = new BreadthFirstSearch();
-tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(3);
-tree.insert(8);
-tree.insert(20);
-console.log(tree.DFSPreOrder())
-// console.log(tree.BFS())
-console.log(tree.DFSPostOrder())
-console.log(tree.DFSInOrder())
+let heap = new MaxBinaryHeap();
+heap.insert(55)
+heap.insert(44)
+heap.insert(47)
+console.log(heap)
