0c9d153ad6591045f4885635fdf402be82a29b93
bisrat <bisrat.amtataw@gmail.com>
2022-08-08 11:22:11 -0400

    Aug 8

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -2060,65 +2060,86 @@ const { Query } = require("mongoose")
 // 3. Retrieves the key-value pair in the hash table
 // 4. If the key isn't found, returns undefined
 
-class HashTable{
-    constructor(size =53){
-        this.keyMap = new Array(size);
+// class HashTable{
+//     constructor(size =53){
+//         this.keyMap = new Array(size);
+//     }
+//     _hash(key){
+//         let total = 0;
+//     let WEIRD_PRIME = 31;
+//     for (let i=0; i<Math.min(key.length, 100); i++){
+//         let char = key[i];
+//         let value = char.charCodeAt(0)-96;
+//         total = (total * WEIRD_PRIME + value)% this.keyMap.length;
+//     }
+//     // console.log(total)
+//     return total;
+//     }
+//     set (key, value){
+//         let index = this._hash(key);
+//         if(!this.keyMap[index]){
+//             this.keyMap[index]=[];
+//         }
+//         this.keyMap[index].push([key, value])
+//         // return index;
+//     }
+//     get(key){
+//         let index = this._hash(key);
+//         if(this.keyMap[index]){
+//             // return this.keyMap([index])
+//             for( let i= 0; i<this.keyMap[index].length;i++){
+//                 if(this.keyMap[index][i][0]===key){
+//                     return this.keyMap[index][i]
+//                 }
+//             }
+//         }
+//         return undefined
+//     }
+//     values(){
+//         let valuesArr = [];
+//         for(let i=0; i<this.keyMap.length;i++){
+//             if(this.keyMap[i]){
+//                 for(let j=0; j<this.keyMap[i].length;j++){
+//                     if(!valuesArr.includes(this.keyMap[i][j][1])){
+//                     valuesArr.push(this.keyMap[i][j][1])
+//                     }
+//                 }
+//             }
+//         }
+//         return valuesArr;
+//     }
+// }
+// let ht= new HashTable();
+// ht.set("Hello world", "goodbye!")
+// ht.set("French", "fries")
+// ht.set("hey", "everyone")
+// ht.set("salmon", "#FA8072")
+// ht.set("Yellow", "##FFFF00")
+// ht.set("Violet", "##FFFF00")
+// // console.log(ht)
+// ht.set("ARE WE DONE", "YES")
+
+// console.log(ht.get("Yellow"))
+// console.log(ht.values())
+// console.log(ht.get("ARE WE DONE"))
+
+// Graph adding a vertex
+// Write a method called addVertex, which accepts a name of a vertex
+// It should add a key to the adjacency list with the name of the vertex and set its value to be an empty array.
+
+class Graph{
+    constructor(){
+        this.adjancencyList = {};
     }
-    _hash(key){
-        let total = 0;
-    let WEIRD_PRIME = 31;
-    for (let i=0; i<Math.min(key.length, 100); i++){
-        let char = key[i];
-        let value = char.charCodeAt(0)-96;
-        total = (total * WEIRD_PRIME + value)% this.keyMap.length;
-    }
-    // console.log(total)
-    return total;
-    }
-    set (key, value){
-        let index = this._hash(key);
-        if(!this.keyMap[index]){
-            this.keyMap[index]=[];
-        }
-        this.keyMap[index].push([key, value])
-        // return index;
-    }
-    get(key){
-        let index = this._hash(key);
-        if(this.keyMap[index]){
-            // return this.keyMap([index])
-            for( let i= 0; i<this.keyMap[index].length;i++){
-                if(this.keyMap[index][i][0]===key){
-                    return this.keyMap[index][i]
-                }
-            }
-        }
-        return undefined
-    }
-    values(){
-        let valuesArr = [];
-        for(let i=0; i<this.keyMap.length;i++){
-            if(this.keyMap[i]){
-                for(let j=0; j<this.keyMap[i].length;j++){
-                    if(!valuesArr.includes(this.keyMap[i][j][1])){
-                    valuesArr.push(this.keyMap[i][j][1])
-                    }
-                }
-            }
-        }
-        return valuesArr;
+    addVertex(vertex){
+        if(!this.adjancencyList[vertex]) 
+        this.adjancencyList[vertex] =[];
     }
 }
-let ht= new HashTable();
-ht.set("Hello world", "goodbye!")
-ht.set("French", "fries")
-ht.set("hey", "everyone")
-ht.set("salmon", "#FA8072")
-ht.set("Yellow", "##FFFF00")
-ht.set("Violet", "##FFFF00")
-// console.log(ht)
-ht.set("ARE WE DONE", "YES")
 
-console.log(ht.get("Yellow"))
-console.log(ht.values())
-console.log(ht.get("ARE WE DONE"))
+let g = new Graph()
+g.addVertex("Tokyo")
+g.addVertex("Tokyo")
+g.addVertex("DC")
+
+console.log(g)
