34d87abc8c498909fc9707f7fd110ba0ce641184
bisrat <bisrat.amtataw@gmail.com>
2022-08-08 11:44:14 -0400

    Aug 8

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -2135,11 +2135,26 @@ class Graph{
         if(!this.adjancencyList[vertex]) 
         this.adjancencyList[vertex] =[];
     }
+    // Adding an Edge 
+    // - This function should accept two vertices, we can call them vertex1 and vertex2
+    // The function should find in the adjacency list the key of vertex 1 and push vertex2 to the array
+    // The function should find in the adjacency list the key of vertex2 and push vertex1 to the array 
+    // Don't worry about handling errors/invalid vertices
+    addEdge(v1,v2){
+         this.adjancencyList[v1].push(v2);
+         this.adjancencyList[v2].push(v1);
+    }
+    removeEdge(v1,v2){
+        this.adjancencyList[v1] = this.adjancencyList[v1].filter( v=> v!==v2)
+        this.adjancencyList[v2] = this.adjancencyList[v2].filter( v=> v!==v1)
+   }
 }
-
 let g = new Graph()
 g.addVertex("Tokyo")
-g.addVertex("Tokyo")
-g.addVertex("DC")
+g.addVertex("Dallas")
+g.addVertex("Aspen")
 
+g.addEdge("Tokyo", "Dallas")
+g.addEdge("Aspen", "Dallas")
+g.removeEdge("Aspen", "Dallas")
 console.log(g)
