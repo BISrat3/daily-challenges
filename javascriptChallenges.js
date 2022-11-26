5d87359ee8af7fdfdce7ac904a371165520a1500
bisrat <bisrat.amtataw@gmail.com>
2022-11-25 22:50:56 -0600

    string to integer

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -2783,27 +2783,38 @@ return (
 ) */}
 
 
-var addTwoNumbers = function() {
-  let l1= [9,9,9,9,9,9,9]
-  let l2 = [9,9,9,9]
+// var addTwoNumbers = function() {
+//   let l1= [9,9,9,9,9,9,9]
+//   let l2 = [9,9,9,9]
   
-  function getNum(list){
-    const numStr = list 
-    .reverse()
-    .join("")
-    return Number(numStr)
-  }
+//   function getNum(list){
+//     const numStr = list 
+//     .reverse()
+//     .join("")
+//     return Number(numStr)
+//   }
   
-  function getList(num){
-    return num
-    .toString()
-    .split("")
-    .reverse()
-    .map(Number)
-  }
-  const sum = getNum(l1) + getNum(l2)
-  const getOutput = getList (sum) 
-  // console.log(getOutput())
-};
+//   function getList(num){
+//     return num
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .map(Number)
+//   }
+//   const sum = getNum(l1) + getNum(l2)
+//   const getOutput = getList (sum) 
+//   // console.log(getOutput())
+// };
 
-console.log(addTwoNumbers())
+// console.log(addTwoNumbers())
+
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// Read in and ignore any leading whitespace.
+// Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+// Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+// Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+// If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+// Return the integer as the final result.
