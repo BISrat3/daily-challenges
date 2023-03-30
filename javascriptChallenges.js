a0576879efd32510c6933200700373ce54f580bc
bisrat <bisrat.amtataw@gmail.com>
2023-03-29 22:40:45 -0400

    search insert solved

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -3185,3 +3185,20 @@ function searchInsert(numbers, target){
   return i-1
 }
 console.log(searchInsert())
+
+
+// // class Solution {
+//   public int searchInsert(int[] nums, int target) {
+//     int start = 0;
+//     int end = nums.length-1;
+
+//     while (start <= end) {
+//         int mid = start + (end-start)/2;
+//         if (nums[mid] == target) return mid;
+//         else if (nums[mid] > target) end = mid-1;
+//         else start = mid+1;
+//     }
+
+//     return start;
+// }
+// }
